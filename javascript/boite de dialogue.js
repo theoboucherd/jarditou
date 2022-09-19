dee7cc649c6edc4e0396d60791ee1d7f65275c39
Théo Boucher <theo.boucher017@gmail.com>
2022-09-19 16:36:31 +0200

    last modif

---
+++ javascript/boite de dialogue.js
@@ -0,0 +1,9 @@
+var reponse1 = prompt("Quel est votre nom?");
+var reponse2 = prompt("Quel est votre prénom?");
+if (window.confirm("Etes vous un homme?") == true)
+
+{
+
+var sex = "monsieur"
+}
+window.alert("Bonjour " + sex +" "+reponse1+" "+reponse2+"\n\nbienvenue sur notre site web!");
